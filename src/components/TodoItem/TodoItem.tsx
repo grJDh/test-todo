19374977c87cc2bc99f4b76159690b1c43d9b0ef
grJDh <rusmukhametov@gmail.com>
2022-11-05 14:16:21 +0300

    Fixed bug with item overflow

--- src/components/TodoItem/TodoItem.tsx
+++ src/components/TodoItem/TodoItem.tsx
@@ -23,11 +23,12 @@ const ToDoItem = ({ text, id, finished, removeItem, toggleItem }: Props) => {
         <MdDeleteOutline />
       </button>
       <div
-        className={`flex justify-start items-center w-full h-full transition-all peer-hover:bg-red-50 group hover:bg-gray-100`}
+        className={`flex justify-start items-center w-full h-full transition-all peer-hover:bg-red-50 group hover:bg-gray-100
+                    overflow-hidden`}
       >
         <div
-          className={`transition-all border border-gray-300 rounded-full mx-2 w-6 h-6 sm:w-8 sm:h-8 flex justify-center items-center
-                       ${finished ? "border-green-200" : ""}`}
+          className={`transition-all border border-gray-300 rounded-full mx-2 h-6 min-w-[2rem] sm:w-8 sm:h-8 flex justify-center items-center
+                    ${finished ? "border-green-200" : ""}`}
         >
           <BsCheck2
             className={` transition-all duration-300
@@ -38,7 +39,7 @@ const ToDoItem = ({ text, id, finished, removeItem, toggleItem }: Props) => {
         </div>
         <p
           className={`relative before:absolute before:top-[55%] before:left-0 before:h-[2px]
-                      transition-all duration-200 before:duration-200
+                      transition-all duration-200 before:duration-200 text-ellipsis overflow-hidden
                       ${
                         finished
                           ? "before:w-full text-gray-300 before:bg-gray-300"
